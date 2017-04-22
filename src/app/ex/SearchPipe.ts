e8edd6824c0ed8e5a4339ef0f9a6b32ddf342fc6
biniam1990 <biniamw1990@gmail.com>
2017-04-22 06:26:50 -0500

    change search to filter

--- src/app/ex/SearchPipe.ts
+++ src/app/ex/SearchPipe.ts
@@ -6,21 +6,8 @@ import {Pipe, PipeTransform} from "@angular/core";
   name: 'search'
 })
 export class SearchPipe implements PipeTransform {
-  posts;
-  newposts = [];
   transform(posts, value) {
-    this.posts = posts;
-    this.newposts = [];
-    this.search(value);
-    return this.newposts;
+     return posts.filter(post=>(post.title.indexOf(value)>=0||value.length==0));
   }
 
-  search(value: string ) {
-    console.log(value);
-      for ( const key in this.posts) {
-      if( this.posts[key].title.indexOf(value) >= 0  || value.length === 0) {
-        this.newposts.push(this.posts[key]);
-      }
-    }
-  }
 }
